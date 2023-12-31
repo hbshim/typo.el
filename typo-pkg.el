020bffbfdf43d1eb56bda94d884ccea948511a1d
Hyungbo Shim <hbshim@gmail.com>
2023-12-31 14:21:47 -0500

    initial installation

---
+++ typo-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from typo.el  -*- no-byte-compile: t -*-
+(define-package "typo" "1.1" "Minor mode for typographic editing" 'nil :kind vc :commit "173ebe4fc7ac38f344b16e6eaf41f79e38f20d57" :authors '(("Jorgen Schaefer" . "forcer@forcix.cx")) :maintainers '(("Jorgen Schaefer" . "forcer@forcix.cx")) :maintainer '("Jorgen Schaefer" . "forcer@forcix.cx") :keywords '("convenience" "wp") :url "https://github.com/jorgenschaefer/typoel")
